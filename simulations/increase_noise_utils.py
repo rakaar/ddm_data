6140c62c2262b93ad307865de5102555d68ef637
rakaar <rka87338@gmail.com>
2024-11-07 20:26:21 +0000

    bound decrease

---
+++ simulations/increase_noise_utils.py
@@ -0,0 +1,101 @@
+import numpy as np
+from scipy.special import erf
+import os
+
+import random
+from numba import jit
+
+# simulation part
+
+def psiam_tied_data_gen_wrapper_noise_change(V_A, theta_A, ABL_arr, ILD_arr, rate_lambda, T_0, theta_E, Z_E, t_A_aff, t_E_aff, t_motor, L, \
+                                t_stim_and_led_tuple, new_noise, iter_num, N_print, dt):
+    ABL = random.choice(ABL_arr)
+    ILD = random.choice(ILD_arr)
+    
+    # random element from t_stim_and_led_tuple
+    t_stim, t_led = t_stim_and_led_tuple[np.random.randint(0, len(t_stim_and_led_tuple))]
+
+    is_LED_trial = np.random.rand() < 1/3
+    # print after every N_print iterations
+    if iter_num % N_print == 0:
+        print(f'os id: {os.getpid()}, In iter_num: {iter_num}, ABL: {ABL}, ILD: {ILD}, t_stim: {t_stim}')
+
+    choice, rt, is_act = simulate_psiam_tied_noise_change(V_A, theta_A, ABL, ILD, rate_lambda, T_0, theta_E, Z_E, t_stim, t_A_aff, t_E_aff, t_motor, L, is_LED_trial, t_led, new_noise, dt)
+    return {'choice': choice, 'rt': rt, 'is_act': is_act ,'ABL': ABL, 'ILD': ILD, 't_stim': t_stim, 't_led': t_led, 'is_LED_trial': is_LED_trial}
+
+@jit
+def simulate_psiam_tied_noise_change(V_A, theta_A, ABL, ILD, rate_lambda, T_0, theta_E, Z_E, t_stim, t_A_aff, t_E_aff, t_motor, L, is_LED_trial, t_led , new_noise, dt):
+    AI = 0; DV = Z_E; t = 0; dB = dt**0.5
+    
+    chi = 17.37; q_e = 1
+    theta = theta_E * q_e
+    mu = (2*q_e/T_0) * (10**(rate_lambda * ABL/20)) * np.sinh(rate_lambda * ILD/chi)
+    sigma = np.sqrt( (2*(q_e**2)/T_0) * (10**(rate_lambda * ABL/20)) * np.cosh(rate_lambda * ILD/ chi) )
+    
+    is_act = 0
+
+    if is_LED_trial:
+        new_sigma = sigma + new_noise
+
+    while True:
+        if t*dt >= t_led and is_LED_trial:
+            sigma = new_sigma
+
+        if t*dt > t_stim + t_E_aff:
+            DV += mu*dt + sigma*np.random.normal(0, dB)
+        
+        if t*dt > t_A_aff:
+            AI += V_A*dt + np.random.normal(0, dB)
+        
+        t += 1
+        
+        if DV >= theta:
+            choice = +1; RT = t*dt + t_motor
+            break
+        elif DV <= -theta:
+            choice = -1; RT = t*dt + t_motor
+            break
+        
+        if AI >= theta_A:
+            is_act = 1
+            AI_hit_time = t*dt
+            # if t*dt > t_stim - t_motor:
+            while t*dt <= (AI_hit_time + t_E_aff + t_motor):#  u can process evidence till stim plays
+                if t*dt > t_stim + t_E_aff: # Evid accum wil begin only after stim starts and afferent delay
+                    DV += mu*dt + sigma*np.random.normal(0, dB)
+                    if DV >= theta:
+                        DV = theta
+                        break
+                    elif DV <= -theta:
+                        DV = -theta
+                        break
+                t += 1
+            
+            break
+        
+        
+    if is_act == 1:
+        RT = AI_hit_time + t_motor
+        # if DV != 0:
+        if DV >= (1 + (L/2) - 1)*theta:
+            choice = 1
+        elif DV <= (1 - (L/2) - 1)*theta:
+            choice = -1
+        else:
+            prob_hit_up = (1/L)*((DV/theta) + 1) + (0.5 - (1/L))            
+            if np.random.rand() <= prob_hit_up:
+                choice = 1
+            else:
+                choice = -1
+        # if DV > 0:
+        #     choice = 1
+        # elif DV < 0:
+        #     choice = -1
+        # else: # if DV is 0 because stim has not yet been played, then choose right/left randomly
+        #     randomly_choose_up = np.random.rand() >= 0.5
+        #     if randomly_choose_up:
+        #         choice = 1
+        #     else:
+        #         choice = -1       
+    
+    return choice, RT, is_act
