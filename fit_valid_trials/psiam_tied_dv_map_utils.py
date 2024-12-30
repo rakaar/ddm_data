6b66811d5d88e6de86232914acf63cde102a0554
Raghavendra Kaushik <rka87338@gmail.com>
2024-12-30 13:18:09 +0000

    try left censoring before 0.2

--- fit_valid_trials/psiam_tied_dv_map_utils.py
+++ fit_valid_trials/psiam_tied_dv_map_utils.py
@@ -7,6 +7,79 @@ import random
 
 # simulation part
 
+def psiam_tied_data_gen_wrapper_V2(V_A, theta_A, ABL_arr, ILD_arr, rate_lambda, T_0, theta_E, Z_E, t_A_aff, t_E_aff, t_motor, L, \
+                                t_stim, iter_num, N_print, dt):
+    ABL = random.choice(ABL_arr)
+    ILD = random.choice(ILD_arr)
+    
+    # print after every N_print iterations
+    if iter_num % N_print == 0:
+        print(f'In iter_num: {iter_num}, ABL: {ABL}, ILD: {ILD}, t_stim: {t_stim}')
+
+
+    choice, rt, is_act = simulate_psiam_tied(V_A, theta_A, ABL, ILD, rate_lambda, T_0, theta_E, Z_E, t_stim, t_A_aff, t_E_aff, t_motor, L, dt)
+    return {'choice': choice, 'rt': rt, 'is_act': is_act ,'ABL': ABL, 'ILD': ILD, 't_stim': t_stim}
+
+
+def simulate_psiam_tied(V_A, theta_A, ABL, ILD, rate_lambda, T_0, theta_E, Z_E, t_stim, t_A_aff, t_E_aff, t_motor, L, dt):
+    AI = 0; DV = Z_E; t = t_A_aff; dB = dt**0.5
+    
+    chi = 17.37; q_e = 1
+    theta = theta_E * q_e
+    mu = (2*q_e/T_0) * (10**(rate_lambda * ABL/20)) * np.sinh(rate_lambda * ILD/chi)
+    sigma = np.sqrt( (2*(q_e**2)/T_0) * (10**(rate_lambda * ABL/20)) * np.cosh(rate_lambda * ILD/ chi) )
+    
+    is_act = 0
+    while True:
+        if t > t_stim + t_E_aff:
+            DV += mu*dt + sigma*np.random.normal(0, dB)
+        
+        AI += V_A*dt + np.random.normal(0, dB)
+        
+        t += dt
+        
+        if DV >= theta:
+            choice = +1; RT = t + t_motor
+            break
+        elif DV <= -theta:
+            choice = -1; RT = t + t_motor
+            break
+        
+        if AI >= theta_A:
+            is_act = 1
+            AI_hit_time = t
+            while t <= (AI_hit_time + t_E_aff + t_motor):#  u can process evidence till stim plays
+                if t > t_stim + t_E_aff: # Evid accum wil begin only after stim starts and afferent delay
+                    DV += mu*dt + sigma*np.random.normal(0, dB)
+                    if DV >= theta:
+                        DV = theta
+                        break
+                    elif DV <= -theta:
+                        DV = -theta
+                        break
+                t += dt
+            
+            break
+        
+        
+    if is_act == 1:
+        RT = AI_hit_time + t_motor
+        # if DV != 0:
+        if DV >= (1 + (L/2) - 1)*theta:
+            choice = 1
+        elif DV <= (1 - (L/2) - 1)*theta:
+            choice = -1
+        else:
+            prob_hit_up = (1/L)*((DV/theta) + 1) + (0.5 - (1/L))            
+            if np.random.rand() <= prob_hit_up:
+                choice = 1
+            else:
+                choice = -1
+             
+    
+    return choice, RT, is_act
+
+
 def psiam_tied_data_gen_wrapper(V_A, theta_A, ABL_arr, ILD_arr, rate_lambda, T_0, theta_E, Z_E, t_A_aff, t_E_aff, t_motor, L, \
                                 t_stim_0, t_stim_tau, iter_num, N_print, dt):
     ABL = random.choice(ABL_arr)
